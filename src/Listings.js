1aa115045db0d3d92f02d4bc1ffd191fd0f21d5d
David Lee <davidlee980804@gmail.com>
2021-02-02 16:25:47 -0800

    Implemented getting all listings, with search form WIP

--- src/Listings.js
+++ src/Listings.js
@@ -1,22 +1,51 @@
 // import Search from './Search';
 import { ListGroup } from 'react-bootstrap';
+import {useState, useEffect} from "react";
 import Listing from './Listing';
+import ShareBnBApi from "./api/api";
+import SearchForm from './SearchForm';
+// import { Link } from 'react-router-dom';
 
-// import Messages from './Messages';
 
 
 /**  Listings Component
  * Props:
  * State:
+ *  - listings: array of objects containing listing information
+ *    [{id, name, price, capacity, zipcode}, ...]
  *
  * App -> Listings
  */
 function Listings(){
+  const [listings, setListings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  useEffect(function getAllListingsOnMount() {
+    async function getAllListings() {
+      let listings = await ShareBnBApi.getAllListings();
+      setListings(listings);
+      setIsLoading(false);
+    }
+    if (isLoading) getAllListings();
+  }, [isLoading]);
+
   return(
     <div>
       <h2>Book a covid-safe outdoor location!</h2>
+      <SearchForm />
       <ListGroup>
-        <Listing/>
+        {listings.map(l => (
+          <Listing
+            key={l.id}
+            id={l.id}
+            name={l.name}
+            price={l.price}
+            capacity={l.capacity}
+            zipcode={l.zipcode}
+            />
+          ))
+        }
       </ListGroup>
     </div>
   );
---
+++ src/SearchForm.js
@@ -0,0 +1,44 @@
+import {Form, Button} from 'react-bootstrap';
+import {useState} from "react";
+
+/**  SearchForm Component
+ * Props: name, price, zipcode, capacity, photoUrl
+ * State:
+ *
+ * App -> Listings -> SearchForm
+ */
+function SearchForm(){
+  const [searchTerm, setSearchTerm] = useState("");
+
+  /** Tell parent to filter */
+  function handleSubmit(evt) {
+    // take care of accidentally trying to search for just spaces
+    evt.preventDefault();
+    // searchFor(searchTerm.trim() || undefined);
+    setSearchTerm(searchTerm.trim());
+  }
+
+  /** Update form fields */
+  function handleChange(evt) {
+    setSearchTerm(evt.target.value);
+  }
+
+  return(
+    <Form inline onSubmit={handleSubmit}>
+        <Form.Label htmlFor="SearchForm-searchInput" srOnly>
+          Search Term
+        </Form.Label>
+        <Form.Control
+          className="mb-2 mr-sm-2"
+          id="SearchForm-searchInput"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <Button type="submit" className="mb-2">
+          Submit
+        </Button>
+    </Form>
+  );
+}
+export default SearchForm;
