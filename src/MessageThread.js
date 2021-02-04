c0ba82ba8e88f36e965064e09a38fc5c0bf48135
J. Sterling Bishop <jsterlingbishop@gmail.com>
2021-02-04 14:24:54 -0800

    messaging component placeholders

---
+++ src/MessageThread.js
@@ -0,0 +1,4 @@
+function MessageThread({from_user}){
+  return <div>Message Thread from: {from_user}</div>;
+}
+export default MessageThread;
--- src/Messages.js
+++ src/Messages.js
@@ -1,9 +1,5 @@
-// import { NavLink }from 'react-router-dom';
-// import Search from './Search';
-// import HomePage from './HomePage';
-// import Listings from './Listings';
-// import Messages from './Messages';
-// import NotFound from './NotFound';
+import {Tab, Row, Col, ListGroup} from 'react-bootstrap';
+import MessageThread from './MessageThread';
 
 /**  Messages Component
  * Props:
@@ -11,11 +7,37 @@
  *
  * App -> Messages
  */
-function Messages(){
+function Messages({messages}){
+
   return(
     <div className="text-white">
       <h1> Messages! </h1>
-      <p>Messages go here</p>
+      <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
+      <Row>
+        <Col sm={2}>
+          <ListGroup>
+            {/* Map over all threads and display a link to the thread here  */}
+            <ListGroup.Item action href="#link1">
+              Link 1
+            </ListGroup.Item>
+            <ListGroup.Item action href="#link2">
+              Link 2
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col sm={8}>
+          <Tab.Content>
+            {/* Map over all threads and display the thread here  */}
+            <Tab.Pane eventKey="#link1">
+              <MessageThread from_user="david"/>
+            </Tab.Pane>
+            <Tab.Pane eventKey="#link2">
+              <MessageThread from_user="sterling"/>
+            </Tab.Pane>
+          </Tab.Content>
+        </Col>
+      </Row>
+    </Tab.Container>
     </div>
   );
 }
